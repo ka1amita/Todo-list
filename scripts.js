66f9ef31b184ce4fa20f009eeae2828574c59471
ka1amita <kalamatej@gmail.com>
2023-03-28 21:55:50 +0200

    clearing comments

--- scripts.js
+++ scripts.js
@@ -1,15 +1,4 @@
 // adding a new todo
-let newTodoCounter = 1
-// $('.addTodo').on('click', function() {
-//     let newTodoText = $('input').val()
-//     console.log('new todo is: ' + newTodoText)
-//     console.log('new todo id is: ' +`id="todo`+newTodoCounter+`"`)
-//     $('.todosContainer').append(`<div class='todoContainer' id='todoContainer`+newTodoCounter+`'><p class='todoItem' id='todoItem`+newTodoCounter+`'>`+newTodoText+`</p><button class='toDelete' id="toDelete`+newTodoCounter+`">Del</button><button class='toDone' id="toDone`+newTodoCounter+`">Done</button> </div>`)
-//     // $('ul').append($('<li>', {class: 'todoItem', id:"todoItem"+newTodoCounter, text: newTodoText}))
-//     newTodoCounter++
-// })
-
-
 $('button.addTodo').on('click', function() {
     let newTodoText = $('input').val()
     $('.todosContainer').append(
@@ -26,9 +15,6 @@ $('button.addTodo').on('click', function() {
 
 // striking all todoX after clicking on a todo list item
 
-// Event binding on dynamically created elements; delegated event handlers 
-// https://stackoverflow.com/questions/203198/event-binding-on-dynamically-created-elements
-
 $('.todosContainer').on('click', '.toDone', function(event) {
     $(this).siblings('.todoItem').toggleClass('done')
     $(this).toggleClass('toDoDone')
@@ -38,18 +24,3 @@ $('.todosContainer').on('click', '.toDone', function(event) {
 $('.todosContainer').on('click', '.toDelete', function(event) {
     $(this).parent().remove()
 });
-
-
-// this approach doesn't wotk with dynamic alements
-
-// $('li.todoItem').on('click', function () {
-//     $(this).toggleClass('done')
-// })
-
-
-// onclick="struck(this)" and function struck(ele) {}
-// https://stackoverflow.com/questions/47795202/how-to-strike-through-a-list-item-without-disturbing-other-items-in-a-html-list
-
-// function struck(ele) {
-//     ele.style = "text-decoration:line-through";
-//   }
